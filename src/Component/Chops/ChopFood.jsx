95bde13386e5348d57c210cc308651e212dd0196
onakoyakayode <onakoyakayode@gmail.com>
2022-10-14 13:26:52 +0100

    rechecking

--- src/Component/Chops/ChopFood.jsx
+++ src/Component/Chops/ChopFood.jsx
@@ -1,4 +1,4 @@
-export default [
+const Food = [
     {
         image: "https://images.unsplash.com/photo-1574085733277-851d9d856a3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGVzc2VydHxlbnwwfDB8MHx8&auto=format&fit=crop&w=600&q=60",
         title: "Doughnut With Toppings",
